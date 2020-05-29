41c8f40162f773546e5042b52b78272bc820e1a8
Shivam Sharma <shivamrk1995@gmail.com>
2020-05-29 14:13:02 +0530

    Add files via upload

---
+++ Notebook for coursera_capstone.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # this notebook is used for coursera_capstone project.
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
